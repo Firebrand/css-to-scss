6c448b1b623daceca6ca3547b24a37cc7aa685df
saltypickles@yahoo.com <saltypickles@yahoo.com>
2018-06-13 01:59:48 -0500

    Fourth

--- bin/calculatoor.js
+++ bin/calculatoor.js
@@ -6,15 +6,14 @@ const calculatoor = require('../lib/');
 const fs = require('fs');
 
 
-args
-  .option('open', 'Open a file for processing');
+args.option('open', 'Open a file for processing');
 
 const flags = args.parse(process.argv, { name: 'calculatoor' });
 
 
 if (fs.existsSync(flags.open)) {
-  calculatoor.openFile(flags.open);
+  calculatoor.main(flags.open);
 } else {
-  console.log(chalk.red('There was an error trying to read that file name'));
+  console.log(chalk.red(`Could not open "${flags.open}"`));
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,22 @@ const index = require('../lib/index');
 
 
 describe('Index', () => {
-  it('Open file', () => index.openFile('./test/test.txt').then((result) => { return assert.equal(result, ['+5', '+6', '-9', '+20', '*300', '+3', '/2', '*45', '+190', '-12', '-14', '-13']); }));
-});
+  it('Open test file and convert to array', () => index._openFile('./test/test.txt').then((result) => {
+    assert.deepEqual(result, ['+5',
+      '+6',
+      '-9',
+      '+20',
+      '*300',
+      '+3',
+      '/2',
+      '*45',
+      '+190',
+      '-12',
+      '-14',
+      '-13']);
+  }));
 
+  it('Run main function and calculate all values in test file', () => index.main('./test/test.txt').then((result) => {
+    assert.equal(result, 148718.5);
+  }));
+});
